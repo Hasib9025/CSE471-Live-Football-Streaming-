ba9c5bdbf5e8890803186195cabd8416a402a5f5
ruhanibro <98678582+ruhanibro@users.noreply.github.com>
2023-11-18 14:07:04 +0600

    Add files via upload

---
+++ notification.js
@@ -0,0 +1,13 @@
+// pages/notification.js
+import React from 'react';
+
+const NotificationPage = () => {
+  return (
+    <div>
+      <h1>Notification Page</h1>
+      <p>This is the notification page content.</p>
+    </div>
+  );
+};
+
+export default NotificationPage;
---
+++ subscription.js
@@ -0,0 +1,13 @@
+// pages/subscribe.js
+import React from 'react';
+
+const SubscribePage = () => {
+  return (
+    <div>
+      <h1>Subscribe Page</h1>
+      {/* Your subscribe page content */}
+    </div>
+  );
+};
+
+export default SubscribePage;
